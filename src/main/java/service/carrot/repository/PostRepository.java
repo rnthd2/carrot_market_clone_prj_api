cca7a500209a3784e300c041f3cc2609947c6c5c
rnthd2 <rnthd2@naver.com>
2021-04-05 15:36:42 +0900

    post(글/상품) photo api 개발

---
+++ src/main/java/service/carrot/repository/PostRepository.java
@@ -0,0 +1,20 @@
+package service.carrot.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+import service.carrot.domain.dao.Post;
+
+import java.util.List;
+
+@Repository
+@Transactional
+public interface PostRepository extends JpaRepository<Post, Integer> {
+
+    @Override
+    List<Post> findAllById(Iterable<Integer> integers);
+
+    Post findById(Long id);
+
+    void deleteById(Long id);
+}
---
+++ src/main/java/service/carrot/service/PostService.java
@@ -0,0 +1,69 @@
+package service.carrot.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import service.carrot.api.PostController;
+import service.carrot.domain.dao.*;
+import service.carrot.repository.MemberRepository;
+import service.carrot.repository.PostRepository;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class PostService {
+
+    private final EntityManager em;
+
+    private final PostRepository postRepository;
+
+    @Transactional(readOnly = true)
+    public Post findById(Long id) {
+        return postRepository.findById(id);
+    }
+
+    //todo area.code별 조회 필요
+    //todo qQuery 추가 필요
+    @Transactional(readOnly = true)
+    public List<Post> findPostList(String code) {
+        return postRepository.findAll(Sort.by(Sort.Direction.ASC,"id"));
+    }
+
+    public Long registPost(Post post) {
+        em.persist(post);
+        return post.getId();
+    }
+
+    public List<Long> updatePostList(List<Post> postList) {
+        return postList.stream()
+            .map(post -> updatePost(post))
+            .collect(Collectors.toList());
+
+    }
+
+    public Long updatePost(Post post) {
+        Post findPost = postRepository.findById(post.getId());
+        findPost.setTitle(post.getTitle());
+        findPost.setMainText(post.getMainText());
+        findPost.setPostCategory(post.getPostCategory());
+        findPost.setPostStatus(post.getPostStatus());
+        findPost.setArea(post.getArea());
+        findPost.setPrice(post.getPrice());
+        findPost.setPostPhotos(post.getPostPhotos());
+        return findPost.getId();
+    }
+
+    public Long deletePost(Long id) {
+        postRepository.deleteById(id);
+        return id;
+    }
+
+//    @Transactional //default readOnly false라 읽기 전용이 아닌경우 이렇게
+
+}
